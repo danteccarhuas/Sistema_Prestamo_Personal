e1ce723d01b2da0212f9a44e728ee1d7a41a19ef
dante ccarhuas <ccarhuas.dante@gmail.com>
2016-06-19 15:34:48 -0500

    agregue jquery

--- SPP/App_Start/RouteConfig.cs
+++ SPP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SPP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Seguridad", action = "Loguin", id = UrlParameter.Optional }
             );
         }
     }
